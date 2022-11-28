eb2b8ff0b2704f526d57cbc51029c0398f20aafa
jimist <arash.jeem@gmail.com>
2022-11-28 06:08:27 +0100

    some opiniated reformattings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,37 @@
+import { resolve } from 'path'
+
 export default defineNuxtConfig({
+  // sccoped system configuration
+  alias: {
+    // https://nuxtjs.org/docs/configuration-glossary/configuration-alias
+    '~~': resolve(__dirname, './'),
+    'img': '~~/src/asset/image',
+  },
+  dir: {
+    // https://nuxtjs.org/docs/configuration-glossary/configuration-dir/
+    pages: 'app/page',
+    layouts: 'src/layout',
+    assets: 'src/asset',
+  },
   modules: [
+    // https://nuxtjs.org/docs/configuration-glossary/configuration-modules
     '@vueuse/nuxt',
     '@unocss/nuxt',
     '@pinia/nuxt',
     '@nuxtjs/color-mode',
   ],
+  css: [
+    // https://nuxtjs.org/docs/configuration-glossary/configuration-css
+    '@unocss/reset/tailwind.css',
+  ],
+
+  // unscoped system configuration
   experimental: {
     reactivityTransform: true,
     inlineSSRStyles: false,
   },
-  css: [
-    '@unocss/reset/tailwind.css',
-  ],
+
+  // depnency configuration
   colorMode: {
     classSuffix: '',
   },
